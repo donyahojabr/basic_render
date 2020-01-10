0ae313e8060223bd47f331bb9f42f65bc43563a3
donyahojabr <donya.hojabr@mail.mcgill.ca>
2020-01-10 18:05:09 -0500

    main.cpp and vec3 class

---
+++ cmake-build-debug/CMakeFiles/basic_render.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_render.dir/main.cpp.o"
+  "basic_render.pdb"
+  "basic_render"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
